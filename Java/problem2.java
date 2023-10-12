77001e1d41ce5fd7fdb266a1a0e6217a6b5511d8
Ramkumar-2269 <ramkumarakula84@gmail.com>
2023-10-12 14:43:15 +0530

    Java completed

--- Java/problem2.java
+++ Java/problem2.java
@@ -1,5 +1,32 @@
 package Java;
 
+import java.util.*;
+
 public class problem2 {
-    
+    public static void main(String[] args) {
+        String roman = "IX"; 
+
+        Map<Character, Integer> romanToInt = new HashMap<>();
+        romanToInt.put('I', 1);
+        romanToInt.put('V', 5);
+        romanToInt.put('X', 10);
+        romanToInt.put('L', 50);
+        romanToInt.put('C', 100);
+        romanToInt.put('D', 500);
+        romanToInt.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = roman.length() - 1; i >= 0; i--) {
+            char c = roman.charAt(i);
+            int value = romanToInt.get(c);
+            if (i < roman.length() - 1 && value < romanToInt.get(roman.charAt(i + 1))) {
+                result -= value;
+            } else {
+                result += value;
+            }
+        }
+
+        System.out.println("The integer value of " + roman + " is: " + result);
+    }
 }
---
+++ Java/problem3.java
@@ -0,0 +1,32 @@
+package Java;
+
+import java.util.*;
+
+public class problem3 {
+    public static void main(String[] args) {
+        String input = "Hi my name is ram.";
+        int flag=0;
+
+        input = input.toLowerCase();
+        Set<Character> letters = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                letters.add(c);
+            }
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (!letters.contains(c)) {
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==1){
+            System.out.println("Not Pangram.");
+        }
+        else{
+            System.out.println("Pangram.");
+        }
+        
+    }
+}
